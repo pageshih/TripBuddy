0e403b61931d69f648faffa63c48d2e3a16217fd
PageShih <a0981863141@gmail.com>
2022-04-10 17:18:16 +0800

    add date picker
    refactor folder structure
    
    get user profile
    
    add simple layout
    
    add navigate link
    
    add sign out button and reform utils style
    
    remove uid of router path & add uid context
    
    add card list layout component
    
    add checkIsLogin when uid is undefiened & remove uid when logout
    
    add uid context & round button
    
    add google map & get placeID when click POI
    
    try to add red marker when select POI
    
    hide other poi markers when click poi marker & get place details
    
    change map center when click poi marker
    
    add place detail data to side column
    
    expend side column when click poi marker
    
    add spot into savedSpot collection of firestore
    
    try to get savedSpots
    
    add global style component
    
    make google map function abstraction in googleMap.js
    
    get savedSpots and show on side column
    
    add checkbox of saved spots
    
    show savedSpot detail & marker on map when click savedSpot card
    
    fix savedSpotDetail click event bug
    
    remove selected spots from savedSpots when click delete btn
    
    fix delete spots bug
    
    delete saved spots from firestore when click delete btn
    
    delete selected spots from firestore by writeBatch
    
    add form of addItinerary page
    
    refactor explore component
    
    refactor folder structure
    
    add date picker
    
    add google map & get placeID when click POI
    
    try to add red marker when select POI
    
    hide other poi markers when click poi marker & get place details
    
    change map center when click poi marker
    
    add place detail data to side column
    
    expend side column when click poi marker
    
    add spot into savedSpot collection of firestore
    
    try to get savedSpots
    
    add global style component
    
    make google map function abstraction in googleMap.js
    
    get savedSpots and show on side column
    
    add checkbox of saved spots
    
    show savedSpot detail & marker on map when click savedSpot card
    
    fix savedSpotDetail click event bug
    
    remove selected spots from savedSpots when click delete btn
    
    fix delete spots bug
    
    delete saved spots from firestore when click delete btn
    
    delete selected spots from firestore by writeBatch
    
    add form of addItinerary page
    
    refactor explore component
    
    refactor folder structure
    
    try to add mui library
    
    Revert "refactor folder structure"
    
    This reverts commit bc45d0cf54187c75890363aa07d6d12d24c42857.
    
    add mui x-data-grid library for date picker
    
    add mui x-date-pickers
    
    add date picker

--- src/components/AddItinerary.js
+++ src/components/AddItinerary.js
@@ -1,4 +1,6 @@
 import { useContext, useEffect, useState } from 'react';
+import { AdapterLuxon } from '@mui/x-date-pickers/AdapterLuxon';
+import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
 import { Context } from '../App';
 import { TextInput } from './styledComponents/TextField';
 import { Container } from './styledComponents/Layout';
@@ -9,9 +11,60 @@ import {
   CardWrapper,
 } from './styledComponents/Layout';
 
+function ChooseDate(props) {
+  return (
+    <>
+      <h2>預計要去玩幾天呢？</h2>
+      <LocalizationProvider dateAdapter={AdapterLuxon}>
+        <FlexDiv gap="20px">
+          <DatePicker
+            label="date range"
+            value={props.startDate}
+            onChange={(newDate) => {
+              props.setStartDate(newDate);
+            }}
+            renderInput={({ inputRef, inputProps, InputProps }) => (
+              <FlexDiv alignItems="center">
+                <TextInput
+                  ref={inputRef}
+                  {...inputProps}
+                  placeholder="旅程開始日期"
+                  readOnly
+                />
+                {InputProps?.endAdornment}
+              </FlexDiv>
+            )}
+          />
+          <p>到</p>
+          <DatePicker
+            label="date range"
+            value={props.endDate}
+            onChange={(newDate) => {
+              props.setEndDate(newDate);
+            }}
+            renderInput={({ inputRef, inputProps, InputProps }) => (
+              <FlexDiv alignItems="center">
+                <TextInput
+                  ref={inputRef}
+                  {...inputProps}
+                  placeholder="旅程結束日期"
+                  readOnly
+                />
+                {InputProps?.endAdornment}
+              </FlexDiv>
+            )}
+          />
+        </FlexDiv>
+      </LocalizationProvider>
+    </>
+  );
+}
+
 function AddItinerary({ setWaitingSpots, waitingSpots }) {
   const { uid, setUid } = useContext(Context);
-  const [step, setStep] = useState(0);
+  const [title, setTitle] = useState();
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
   const addOverView = [
     {
       title: '為這趟旅程取個名字吧！',
@@ -31,12 +84,24 @@ function AddItinerary({ setWaitingSpots, waitingSpots }) {
   return (
     <Container maxWidth="1200px" margin="80px auto 0px auto">
       <h2>為這趟旅程取個名字吧！</h2>
-      <TextInput type="text" placeholder="請輸入行程名稱" />
+      <TextInput
+        type="text"
+        placeholder="請輸入行程名稱"
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+      />
       <h2>您選擇了這些景點：</h2>
       <CardWrapper gap="20px">
         {waitingSpots?.map((spot) => {
           return (
-            <Card column gap="20px" position="relative" key={spot.place_id}>
+            <Card
+              column
+              gap="20px"
+              position="relative"
+              basis="350px"
+              key={spot.place_id}>
               <img
                 src={spot.photos[0]}
                 style={{ width: '100%', objectFit: 'cover' }}
@@ -51,7 +116,12 @@ function AddItinerary({ setWaitingSpots, waitingSpots }) {
           );
         })}
       </CardWrapper>
-      <h2>預計要去玩幾天呢？</h2>
+      <ChooseDate
+        startDate={startDate}
+        setStartDate={setStartDate}
+        endDate={endDate}
+        setEndDate={setEndDate}
+      />
     </Container>
   );
 }
