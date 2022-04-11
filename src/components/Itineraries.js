7d73a57915a5da3302024bb233b8d7cae81ee4d6
PageShih <a0981863141@gmail.com>
2022-04-11 12:06:08 +0800

    add card list layout component

--- src/components/Itineraries.js
+++ src/components/Itineraries.js
@@ -1,8 +1,11 @@
+import { CardRow } from '../utils/Layout';
+
 function Itineraries() {
   return (
-    <>
+    <CardRow gap="20px">
+      <img src="https://fakeimg.pl/150/" alt="cover" />
       <h1>Itinerary</h1>
-    </>
+    </CardRow>
   );
 }
 
