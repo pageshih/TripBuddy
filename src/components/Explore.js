a7ec1c362e6e1b9839225f7de7cf614040ec0356
PageShih <a0981863141@gmail.com>
2022-04-12 17:26:15 +0800

    hide other poi markers when click poi marker & get place details

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -5,22 +5,111 @@ import { googleMapApiKey } from '../utils/apiKey';
 import { firebaseAuth, firestore } from '../utils/firebase';
 import { UidContext } from '../App';
 import { RoundButton } from '../utils/Button';
+import { FlexDiv, FlexChildDiv } from '../utils/Layout';
+import markerIcon from '../images/place_black_48dp.svg';
+import '../marker.css';
+
+const featureShowPattern = {
+  default: [
+    {
+      featureType: 'poi',
+      stylers: [{ visibility: 'off' }],
+    },
+    {
+      featureType: 'poi.business',
+      stylers: [{ visibility: 'on' }],
+    },
+    {
+      featureType: 'poi.attraction',
+      stylers: [{ visibility: 'on' }],
+    },
+    {
+      featureType: 'poi.park',
+      stylers: [{ visibility: 'on' }],
+    },
+    {
+      featureType: 'poi.place_of_worship',
+      stylers: [{ visibility: 'on' }],
+    },
+  ],
+  hideAll: [
+    {
+      featureType: 'poi',
+      stylers: [{ visibility: 'off' }],
+    },
+  ],
+};
 
 function Map({ center, zoom }) {
   const ref = useRef();
   const [map, setMap] = useState();
-  const [infoWindow, setInfoWindow] = useState();
+  const [marker, setMarker] = useState();
   useEffect(() => {
     if (ref.current && !map) {
       setMap(new window.google.maps.Map(ref.current, { center, zoom }));
-      setInfoWindow(new window.google.maps.InfoWindow());
     } else {
+      map.setOptions({
+        styles: featureShowPattern.default,
+      });
+    }
+  }, [ref, map, center, zoom]);
+
+  useEffect(() => {
+    if (ref.current && map) {
       window.google.maps.event.addListener(map, 'click', (e) => {
-        console.log(e.latLng.lat(), e.latLng.lng());
+        if (e.placeId) {
+          map.setOptions({
+            styles: featureShowPattern.hideAll,
+          });
+          const placeRequest = {
+            placeId: e.placeId,
+            fields: ['name', 'formatted_address', 'place_id', 'geometry'],
+          };
+          if (marker) {
+            marker.setMap(null);
+          } else {
+            const placeService = new window.google.maps.places.PlacesService(
+              map
+            );
+            placeService.getDetails(placeRequest, (place, status) => {
+              if (
+                status === window.google.maps.places.PlacesServiceStatus.OK &&
+                place &&
+                place.geometry &&
+                place.geometry.location &&
+                place.name
+              ) {
+                setMarker(
+                  new window.google.maps.Marker({
+                    map,
+                    position: place.geometry.location,
+                    label: {
+                      text: place.name,
+                      color: '#de3400',
+                      className: 'label',
+                    },
+                    icon: {
+                      url: markerIcon,
+                      labelOrigin: new window.google.maps.Point(25, -10),
+                      size: new window.google.maps.Size(48, 48),
+                    },
+                  })
+                );
+              }
+            });
+          }
+        } else {
+          if (marker) {
+            marker.setMap(null);
+            map.setOptions({
+              styles: featureShowPattern.default,
+            });
+          }
+        }
         e.stop();
       });
     }
-  }, [ref, map]);
+  }, [map, marker]);
 
   return <div style={{ width: '100%', height: '100vh' }} ref={ref} />;
 }
@@ -50,11 +139,15 @@ function Explore() {
   return (
     <>
       {uid && (
-        <Wrapper apiKey={googleMapApiKey}>
-          <Map center={center} zoom={zoom} />
-          <RoundButton size="48px">候補</RoundButton>
-          <h1>Explore</h1>
-        </Wrapper>
+        <FlexDiv>
+          <FlexChildDiv basis="300px">
+            <RoundButton size="48px">候補</RoundButton>
+            <h1>Explore</h1>
+          </FlexChildDiv>
+          <Wrapper apiKey={googleMapApiKey} libraries={['places']}>
+            <Map center={center} zoom={zoom} />
+          </Wrapper>
+        </FlexDiv>
       )}
     </>
   );
--- src/utils/Layout.js
+++ src/utils/Layout.js
@@ -4,19 +4,30 @@ import { css, jsx } from '@emotion/react';
 
 const FlexDiv = styled.div`
   display: flex;
+  flex-wrap: ${(props) => props.wrap || 'nowrap'};
   align-items: ${(props) => props.alignItems || 'stretch'};
+  justify-content: ${(props) => props.justifyContent || 'flex-start'};
   flex-direction: ${(props) => props.direction || 'row'};
   gap: ${(props) => props.gap || 0};
 `;
-const CardStyle = css`
+
+const FlexChildDiv = styled.div`
+  flex: ${(props) => props.flex || '0 1 auto'};
+  flex-grow: ${(props) => props.grow || '0'};
+  flex-shrink: ${(props) => props.shrink || '1'};
+  flex-basis: ${(props) => props.basis || 'auto'};
+  align-self: ${(props) => props.alignSelf || 'auto'};
+  order: ${(props) => props.order || '0'};
+`;
+const cardStyle = css`
   border: 1px solid lightgray;
   padding: 10px;
   list-style: none;
   display: flex;
 `;
 const CardRow = styled.ul`
-  ${CardStyle}
+  ${cardStyle}
   gap: ${(props) => props.gap || '0px'}
 `;
 
-export { FlexDiv, CardRow };
+export { FlexDiv, CardRow, FlexChildDiv };
