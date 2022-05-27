4ff5bfc7463201f5e183b4b821e504a8c02e1412
PageShih <a0981863141@gmail.com>
2022-05-27 18:39:58 +0800

    refactor Footer

--- src/components/styledComponents/Footer.js
+++ src/components/styledComponents/Footer.js
@@ -2,21 +2,24 @@
 import { css, jsx } from '@emotion/react';
 import { palatte } from './basic/common';
 import { P } from './basic/Text';
-import { FlexDiv } from './Layout';
 
 function Footer() {
   return (
-    <FlexDiv
-      justifyContent="center"
-      padding="15px"
-      addCss={css`
+    <div
+      css={css`
+        justify-content: center;
+        padding: 15px;
         background-color: ${palatte.gray['800']};
         margin-top: auto;
       `}>
-      <P color={palatte.white} fontSize="14px">
+      <P
+        css={css`
+          color: ${palatte.white};
+          font-size: 14px;
+        `}>
         Copyright Ⓒ 2022, Page Shih. All right reserved.
       </P>
-    </FlexDiv>
+    </div>
   );
 }
 
